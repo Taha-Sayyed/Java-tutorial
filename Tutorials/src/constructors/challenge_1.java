f389c471eb901bcac9d0bddd2dab3cbd09176e23
Taha Sayyed <codetaha786@gmail.com>
2025-12-28 14:00:04 +0530

    Add challenge-1

---
+++ Tutorials/src/constructors/challenge_1.java
@@ -0,0 +1,69 @@
+package constructors;
+
+class Account{
+    private String accountNumber;
+    private String holderName;
+    private int phNo;
+
+
+    public Account(String accNo,String name,int phNumber){
+        accountNumber=accNo;
+        holderName=name;
+        phNo=phNumber;
+    }
+}
+
+class loanAccount extends Account{
+    private double loanBalance;
+    public loanAccount(String accNo,String name,int phNumber,double bal){
+        super(accNo,name,phNumber);
+        loanBalance=bal;
+    }
+
+    public void payEMI(double amt){
+        if(loanBalance<amt){
+            System.out.println("Invalid");
+            return;
+        }
+        loanBalance=loanBalance-amt;
+    }
+
+    public double getLoanBalance() {
+        return loanBalance;
+    }
+}
+
+class savingAccount extends Account{
+    private double savingBalance;
+    public savingAccount(String accNo,String name,int phNumber){
+        super(accNo,name,phNumber);
+        savingBalance=0;
+    }
+    public void deposit(double amt){
+        savingBalance+=amt;
+    }
+    public void withdraw(double amt){
+        if(savingBalance<amt){
+            System.out.println("Invalid");
+        }
+        savingBalance-=amt;
+    }
+    public double getSavingBalance() {
+        return savingBalance;
+    }
+}
+
+
+public class challenge_1 {
+    public static void main(String[] args) {
+        loanAccount a=new loanAccount("A023210","Taha Sayyed",995878564,5000);
+        System.out.println("Loan Account balance "+a.getLoanBalance());
+        a.payEMI(200);
+        System.out.println("Loan Account balance "+a.getLoanBalance());
+
+        savingAccount s=new savingAccount("A023210","Taha Sayyed",995878564);
+        s.deposit(1000);
+        s.withdraw(50);
+        System.out.println("Saving Account balance "+s.getSavingBalance());
+    }
+}
