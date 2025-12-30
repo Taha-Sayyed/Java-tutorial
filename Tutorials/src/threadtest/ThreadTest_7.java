9f46622f3fa6dfb3794e2854be414403efb7e9c5
Taha Sayyed <codetaha786@gmail.com>
2025-12-30 15:11:23 +0530

    Updated

--- Tutorials/src/threadtest/ThreadTest_7.java
+++ Tutorials/src/threadtest/ThreadTest_7.java
@@ -13,8 +13,12 @@ class MyClass_4 extends Thread{
 public class ThreadTest_7 {
     public static void main(String[] args) {
         MyClass_4 m=new MyClass_4();
-        m.setDaemon(true); //Depend on main thread
+        m.setDaemon(true); //Daemon threads run in the background
         m.start();
         try{Thread.sleep(2000);}catch (Exception e){}
     }
 }
+
+//The JVM does not wait for daemon threads to finish
+//When all non-daemon threads finish, the JVM exits
+//When the JVM exits, all daemon threads are stopped immediately
