8c3cce979194703f9fc8437a09eacdf586002a62
Taha Sayyed <codetaha786@gmail.com>
2026-01-02 12:58:10 +0530

    Update demo-1

--- Tutorials/src/lambdaexpression/demo_1.java
+++ Tutorials/src/lambdaexpression/demo_1.java
@@ -1,13 +1,13 @@
 package lambdaexpression;
 
-interface MyLambda{
+interface MyLambda{ //Functional Interface
     void display();
 }
 
 public class demo_1 {
     public static void main(String[] args) {
         MyLambda m=()->{
-            System.out.println("Hello World");
+            System.out.println("Hello World"); //Definition of display method
         };
         m.display();
     }
