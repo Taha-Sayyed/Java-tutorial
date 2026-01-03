a43663fd76811a5d071349c27d719a8f1c4858d0
Taha Sayyed <codetaha786@gmail.com>
2026-01-03 15:03:40 +0530

    Add demo 2

---
+++ Tutorials/src/fileexample/demo_2.java
@@ -0,0 +1,24 @@
+package fileexample;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class demo_2 {
+    public static void main(String[] args) {
+        try{
+            FileOutputStream fos=new FileOutputStream("C:\\Users\\Admin\\Desktop\\Java-tutorial\\Tutorials\\src\\fileexample\\test_1");
+            String str="Java is Programming language";
+            byte b[]= str.getBytes();
+            for(byte x:b){
+                fos.write(x);
+            }
+            fos.close();
+        }
+        catch(FileNotFoundException e){
+            System.out.println(e);
+        }
+        catch(IOException e){
+            System.out.println(e);
+        }
+    }
+}
