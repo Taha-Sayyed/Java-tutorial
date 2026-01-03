b3cae9858f4e84e8aa060a714c77cb2092aa37f8
Taha Sayyed <codetaha786@gmail.com>
2026-01-03 14:54:20 +0530

    Add demo 1

---
+++ Tutorials/src/fileexample/demo_1.java
@@ -0,0 +1,21 @@
+package fileexample;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+public class demo_1 {
+    public static void main(String[] args) {
+        try{
+            FileOutputStream fos=new FileOutputStream("C:\\Users\\Admin\\Desktop\\Java-tutorial\\Tutorials\\src\\fileexample\\test_1");
+            String str="Java is good language";
+            fos.write(str.getBytes(StandardCharsets.UTF_8));
+        }
+        catch(FileNotFoundException e){
+            System.out.println(e);
+        }
+        catch(IOException e){
+            System.out.println(e);
+        }
+    }
+}
