0ce810e3fe2cff7460497631df0359a83c4f24d6
Taha Sayyed <codetaha786@gmail.com>
2026-01-04 22:30:27 +0530

    Update demo 4

--- Tutorials/src/fileexample/ByteStreamClass/ByteArrayInputOutputStream/demo_4.java
+++ Tutorials/src/fileexample/ByteStreamClass/ByteArrayInputOutputStream/demo_4.java
@@ -10,5 +10,6 @@ public class demo_4 {
         bos.write('h');
         bos.write('a');
         bos.writeTo(new FileOutputStream("C:\\Users\\Admin\\Desktop\\Java-tutorial\\Tutorials\\src\\fileexample\\ByteStreamClass\\ByteArrayInputOutputStream\\test.txt"));
+        bos.close();
     }
 }
