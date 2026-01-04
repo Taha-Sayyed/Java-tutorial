b8960aac4a2f5356b1942d3bbb623798571b1abd
Taha Sayyed <codetaha786@gmail.com>
2026-01-04 22:17:14 +0530

    Add demo 2

---
+++ Tutorials/src/fileexample/ByteStreamClass/ByteArrayInputOutputStream/demo_2.java
@@ -0,0 +1,12 @@
+package fileexample.ByteStreamClass.ByteArrayInputOutputStream;
+
+import java.io.ByteArrayInputStream;
+
+public class demo_2 {
+    public static void main(String[] args) {
+        byte b[]={'T','a','h','a'};
+        ByteArrayInputStream bis=new ByteArrayInputStream(b);
+        String str=new String(bis.readAllBytes());
+        System.out.println(str);
+    }
+}
