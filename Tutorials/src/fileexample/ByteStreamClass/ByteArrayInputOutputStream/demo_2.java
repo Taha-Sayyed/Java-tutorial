e9e51b735422a8a64ece140c5f503a015a81c00b
Taha Sayyed <codetaha786@gmail.com>
2026-01-04 22:18:21 +0530

    Update demo 2

--- Tutorials/src/fileexample/ByteStreamClass/ByteArrayInputOutputStream/demo_2.java
+++ Tutorials/src/fileexample/ByteStreamClass/ByteArrayInputOutputStream/demo_2.java
@@ -3,10 +3,11 @@ package fileexample.ByteStreamClass.ByteArrayInputOutputStream;
 import java.io.ByteArrayInputStream;
 
 public class demo_2 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
         byte b[]={'T','a','h','a'};
         ByteArrayInputStream bis=new ByteArrayInputStream(b);
         String str=new String(bis.readAllBytes());
         System.out.println(str);
+        bis.close();
     }
 }
