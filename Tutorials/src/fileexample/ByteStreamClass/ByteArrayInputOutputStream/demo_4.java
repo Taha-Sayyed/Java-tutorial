b6b60f41e12cd1f4b3b6254dbcc53f6ae0cca175
Taha Sayyed <codetaha786@gmail.com>
2026-01-04 22:29:29 +0530

    Add demo 4

---
+++ Tutorials/src/fileexample/ByteStreamClass/ByteArrayInputOutputStream/demo_4.java
@@ -0,0 +1,14 @@
+package fileexample.ByteStreamClass.ByteArrayInputOutputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.FileOutputStream;
+
+public class demo_4 {
+    public static void main(String[] args)throws Exception {
+        ByteArrayOutputStream bos=new ByteArrayOutputStream(20);
+        bos.write('T');
+        bos.write('a');
+        bos.write('h');
+        bos.write('a');
+        bos.writeTo(new FileOutputStream("C:\\Users\\Admin\\Desktop\\Java-tutorial\\Tutorials\\src\\fileexample\\ByteStreamClass\\ByteArrayInputOutputStream\\test.txt"));
+    }
+}
