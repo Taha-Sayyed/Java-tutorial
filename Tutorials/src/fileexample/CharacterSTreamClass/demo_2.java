3f6f75af476c45d12e3fbb185be8e6af0e205b0f
Taha Sayyed <codetaha786@gmail.com>
2026-01-04 19:05:43 +0530

    Add demo 2

---
+++ Tutorials/src/fileexample/CharacterSTreamClass/demo_2.java
@@ -0,0 +1,14 @@
+package fileexample.CharacterSTreamClass;
+import java.io.FileReader;
+public class demo_2 {
+    public static void main(String[] args) {
+        try(FileReader fr=new FileReader("C:\\Users\\Admin\\Desktop\\Java-tutorial\\Tutorials\\src\\fileexample\\CharacterSTreamClass\\test")){
+            int x;
+            while((x=fr.read())!=-1){
+                System.out.print((char)x);
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
