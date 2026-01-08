95ec24e9eb3b9565fe93205eda8312d9e49cbc2c
Taha Sayyed <codetaha786@gmail.com>
2026-01-08 20:03:59 +0530

    Update

--- Tutorials/src/Generics/GenericDemo.java
+++ Tutorials/src/Generics/GenericDemo.java
@@ -1,19 +1,24 @@
 package Generics;
 
 public class GenericDemo<T> {
-    T data[]=(T[])new Object[3]; //Array of Object class and typecast as Generic
 
-    public static void main(String[] args) {
-        GenericDemo<String>gd=new GenericDemo<>();
-        gd.data[0]=new String("Hello");
-        gd.data[1]=new String("Taha");
-        gd.data[2]=new String("Sayyed");
+    T[] data;
 
-        String str=gd.data[0];
-        System.out.println(str); //Throwing run time error
-
-
-
+    @SuppressWarnings("unchecked")
+    public GenericDemo() {
+        data = (T[]) new Object[3];
     }
 
+    public static void main(String[] args) {
+
+        GenericDemo<String> gd = new GenericDemo<>();
+
+        gd.data[0] = "Hello";
+        gd.data[1] = "Taha";
+        gd.data[2] = "Sayyed";
+
+        String str = gd.data[0];
+        System.out.println(str);
+    }
 }
+
