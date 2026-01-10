cd8872af64c607b092e27e2b377fd43dfe697c96
Taha Sayyed <codetaha786@gmail.com>
2026-01-10 23:15:21 +0530

    feat: demonstrate multiple ways to iterate over ArrayList using loops, iterators, and forEach

---
+++ Tutorials/src/Collections/Demo2.java
@@ -0,0 +1,56 @@
+package Collections;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
+
+public class Demo2 {
+    public static void main(String[] args) {
+        ArrayList<Integer> a1=new ArrayList<>(List.of(10,20,30,40,50));
+
+        for(int i=0;i<a1.size();i++){
+            System.out.print(a1.get(i)+" ");
+        }
+        System.out.println("\n");
+
+        //for each loop
+        for(Integer x:a1){
+            System.out.print(x+" ");
+        }
+        System.out.println("\n");
+
+        //Using var (No need to mention about Object type)
+        for(var x:a1){
+            System.out.print(x+" ");
+        }
+
+        System.out.println("\n");
+        //Using Iterator class which is generic
+        Iterator<Integer> it=a1.iterator();
+        while(it.hasNext()){
+            System.out.print(it.next()+" ");
+        }
+
+        //Using ListIterator class which is generic
+        System.out.println("\n");
+        ListIterator<Integer> lt=a1.listIterator();
+        while(lt.hasNext()){
+            System.out.print(lt.next()+" ");
+        }
+
+        //Using for loop along with the ListIterator class
+        System.out.println("\n");
+        for(ListIterator<Integer>ot=a1.listIterator();ot.hasNext();){
+            System.out.print(ot.next()+" ");
+        }
+
+        //foreach method of class ArrayList
+        System.out.println("\n");
+        a1.forEach(n-> System.out.print(n+" "));
+
+        System.out.println("\n");
+        a1.forEach(System.out::print);
+
+    }
+}
